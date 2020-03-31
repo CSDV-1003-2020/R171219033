02294cfe4aa308fa4b011266d2de9f3f7d3f460e
Sarthak1306 <sarthak13062001@gmail.com>
2020-03-31 12:21:56 +0530

    Sarthak : num to int, merge conflict

--- calc.cpp
+++ calc.cpp
@@ -1,49 +1,49 @@
-// C++ program to create calculator using 
-// switch statement 
-#include <iostream> 
-using namespace std; 
-  
+// C++ program to create calculator using
+// switch statement
+#include <iostream>
+using namespace std;
+
 // Main program 
-main() 
-{ 
-    char op; 
-    float num1, num2; 
-  
-    // It allows user to enter operator i.e. +, -, *, / 
-    cin >> op; 
-  
-    // It allow user to enter the operands 
-    cin >> num1 >> num2; 
-  
-    // Switch statement begins 
-    switch (op) { 
+main()
+{
+    char op;
+    float int1, int2;
+
+    // It allows user to enter operator i.e. +, -, *, /
+    cin >> op;
+
+    // It allow user to enter the operands
+    cin >> int1 >> int2;
+
+    // Switch statement begins
+    switch (op) {
 <<<<<<< HEAD
-	    
+
 	    // If user enter *
         case '*':
-            cout << num1 * num2;
+            cout << int1 * int2;
             break;
 <<<<<<< HEAD
 =======
 
-	   // If user enter -  
-        case '-': 
-            cout << num1 - num2; 
-            break; 
+	   // If user enter -
+        case '-':
+            cout << int1 - int2;
+            break;
 >>>>>>> subtraction
 =======
-                
-	    // If user enter / 
-        case '/': 
-            cout << num1 / num2; 
-            break; 
+
+	    // If user enter /
+        case '/':
+            cout << int1 / int2;
+            break;
 
 >>>>>>> division
-          
-        // If user enter +  
-        case '+': 
-            cout << num1 + num2; 
-            break; 
+
+        // If user enter +
+        case '+':
+            cout << int1 + int2;
+            break;
 
 
 
@@ -67,23 +67,3 @@ main()
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

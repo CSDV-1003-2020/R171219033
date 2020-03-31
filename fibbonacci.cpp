78941c83ded570d4b844069266757bb520783778
Shivansh <shivanshmp1@gmail.com>
2020-03-31 11:17:09 +0530

    3rd branch commit

--- fibbonacci.cpp
+++ fibbonacci.cpp
@@ -7,7 +7,15 @@ int fib(int x) {
       return(fib(x-1)+fib(x-2));
    }
 }
-
+int main() {
+   int x , i=0;
+   cout << "Enter the number of terms of series : ";
+   cin >> x;
+   cout << "\nFibonnaci Series : ";
+   while(i < x) {
+      cout << " " << fib(i);
+      i++;
    }
    return 0;
 }
+   

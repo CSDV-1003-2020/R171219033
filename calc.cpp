a42f5bffa0644772f7019174f7146cd4ba9d1d27
Sarthak Srivastava <sarthaksrivastavaofficial@gmail.com>
2020-04-07 13:12:32 +0530

    Revert "division01"

--- calc.cpp
+++ calc.cpp
@@ -1,49 +1,35 @@
-// C++ program to create calculator using
-// switch statement
-#include <iostream>
-using namespace std;
-
+// C++ program to create calculator using 
+// switch statement 
+#include <iostream> 
+using namespace std; 
+  
 // Main program 
-main()
-{
-    char op;
-    float int1, int2;
-
-    // It allows user to enter operator i.e. +, -, *, /
-    cin >> op;
-
-    // It allow user to enter the operands
-    cin >> int1 >> int2;
-
-    // Switch statement begins
-    switch (op) {
-<<<<<<< HEAD
-
+main() 
+{ 
+    char op; 
+    float num1, num2; 
+  
+    // It allows user to enter operator i.e. +, -, *, / 
+    cin >> op; 
+  
+    // It allow user to enter the operands 
+    cin >> num1 >> num2; 
+  
+    // Switch statement begins 
+    switch (op) { 
+	    
 	    // If user enter *
         case '*':
-            cout << int1 * int2;
+            cout << num1 * num2;
             break;
-<<<<<<< HEAD
-=======
+                
+	    // If user enter / 
+        case '/': 
+            cout << num1 / num2; 
+            break; 
 
-	   // If user enter -
-        case '-':
-            cout << int1 - int2;
-            break;
->>>>>>> subtraction
-=======
-
-	    // If user enter /
-        case '/':
-            cout << int1 / int2;
-            break;
-
->>>>>>> division
-
-        // If user enter +
-        case '+':
-            cout << int1 + int2;
-            break;
+          
+        
 
 
 
@@ -58,12 +44,18 @@ main()
 
 
 
-	    // If the operator is other than +, -, * or /,
-        // error message will display
-        default:
-            cout << "Error! operator is not correct";
-            break;
-    } // switch statement ends
 
-    return 0;
-}
+
+
+
+
+          
+        // If the operator is other than +, -, * or /,  
+        // error message will display 
+        default: 
+            cout << "Error! operator is not correct"; 
+            break; 
+    } // switch statement ends 
+  
+    return 0;  
+}  
